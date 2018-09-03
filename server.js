8ebf3a715f3fe3a38afb9c9aba625b8011159e3c
Sean Goff <sean.goff@energy.ch>
2018-09-03 02:37:07 +0200

    simplified testcase

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const io = require('socket.io')(12345);
 
 
-function simulateError(data){
-    if(data === 'throw'){
-        throw 'testError';
-    }
-    return data;
-}
-
 io.on('connection', function(socket){
     console.log('connection', socket.id);
     socket.on('reflect', (data)=>{
         console.log('reflect', data);
         try{
-            io.to(socket.id).emit('response', simulateError(data))
+            if(data === 'throw'){
+                io.to(socket.id);
+            }else{
+                io.to(socket.id).emit('response', data)
+            }
             //socket.emit('response', simulateError(data)) //does not cause the bug
         }catch(e){}
     })
